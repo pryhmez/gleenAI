42d32ca9c990a6e543109b77fdb0f546f679504b
PRYHMEZ <edmundisaac16@gmail.com>
2025-02-24 13:32:36 +0100

    fix: changed faster whisper model

--- stream_processor.py
+++ stream_processor.py
@@ -120,11 +120,11 @@ class StreamProcessor:
                         self.end_speech_time = time.time()
                         self.speech_buffer.append(chunk_bytes)
                     elif self.recording_session_active:
-                        print('still in active session waiting for timeout1')
+                        # print('still in active session waiting for timeout1')
                         self.speech_buffer.append(chunk_bytes)
                 else:
                     if self.recording_session_active:
-                        print('still in active session waiting for timout2')
+                        # print('still in active session waiting for timout2')
                         self.speech_buffer.append(chunk_bytes)
 
             except ValueError as e:
