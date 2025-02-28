592f6338ae5fcb153ba71adec5ba7028bfd7cc6b
PRYHMEZ <edmundisaac16@gmail.com>
2025-02-28 22:37:26 +0100

    fix: stream fix

--- common/redis_client.py
+++ common/redis_client.py
@@ -1,5 +1,8 @@
 import redis
+import logging
 
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 # Redis connection
 redis_client = redis.Redis(host='localhost', port=6379, db=0, decode_responses=False)
